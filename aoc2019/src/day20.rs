db4dad991a9ff4236d095829df6ea565dc7a20e8
Mitch Souders <mitch.souders@gmail.com>
2019-12-20 00:16:23 -0800

    Day 20 Part 2

--- aoc2019/src/day20.rs
+++ aoc2019/src/day20.rs
@@ -229,11 +229,206 @@ pub fn p1() -> IoResult<()> {
     
     use petgraph::algo::astar;
     println!("{:?}", astar(&deps, aa, |x| x == zz, |e| *e.weight(), |_| 0).unwrap().0);
-    unimplemented!("Part 1")
+    Ok(())
 }
 
 pub fn p2() -> IoResult<()> {
-    unimplemented!("Part 2")
+    let s = std::fs::read_to_string("input/day20.txt")?;
+//     let s = "         A           
+//          A           
+//   #######.#########  
+//   #######.........#  
+//   #######.#######.#  
+//   #######.#######.#  
+//   #######.#######.#  
+//   #####  B    ###.#  
+// BC...##  C    ###.#  
+//   ##.##       ###.#  
+//   ##...DE  F  ###.#  
+//   #####    G  ###.#  
+//   #########.#####.#  
+// DE..#######...###.#  
+//   #.#########.###.#  
+// FG..#########.....#  
+//   ###########.#####  
+//              Z       
+//              Z  ".to_string();
+//     let s = "                   A               
+//                    A               
+//   #################.#############  
+//   #.#...#...................#.#.#  
+//   #.#.#.###.###.###.#########.#.#  
+//   #.#.#.......#...#.....#.#.#...#  
+//   #.#########.###.#####.#.#.###.#  
+//   #.............#.#.....#.......#  
+//   ###.###########.###.#####.#.#.#  
+//   #.....#        A   C    #.#.#.#  
+//   #######        S   P    #####.#  
+//   #.#...#                 #......VT
+//   #.#.#.#                 #.#####  
+//   #...#.#               YN....#.#  
+//   #.###.#                 #####.#  
+// DI....#.#                 #.....#  
+//   #####.#                 #.###.#  
+// ZZ......#               QG....#..AS
+//   ###.###                 #######  
+// JO..#.#.#                 #.....#  
+//   #.#.#.#                 ###.#.#  
+//   #...#..DI             BU....#..LF
+//   #####.#                 #.#####  
+// YN......#               VT..#....QG
+//   #.###.#                 #.###.#  
+//   #.#...#                 #.....#  
+//   ###.###    J L     J    #.#.###  
+//   #.....#    O F     P    #.#...#  
+//   #.###.#####.#.#####.#####.###.#  
+//   #...#.#.#...#.....#.....#.#...#  
+//   #.#####.###.###.#.#.#########.#  
+//   #...#.#.....#...#.#.#.#.....#.#  
+//   #.###.#####.###.###.#.#.#######  
+//   #.#.........#...#.............#  
+//   #########.###.###.#############  
+//            B   J   C               
+//            U   P   P               ".to_string();
+    
+    let grid = s        
+        .split("\n")
+        .enumerate()
+        .flat_map(
+            move |(y, l)|
+            l.chars().enumerate().map(move |(x, c)| (Point2::new(x as i32, y as i32), c))
+        )
+        .collect::<HashMap<_,char>>();
+
+    
+    let min_x = grid.keys().map(|p| p.x).min().unwrap();
+    let min_y = grid.keys().map(|p| p.y).min().unwrap();
+    let max_x = grid.keys().map(|p| p.x).max().unwrap();
+    let max_y = grid.keys().map(|p| p.y).max().unwrap();
+   
+    use petgraph::Graph;
+    let mut deps = Graph::<Tile, usize, petgraph::Undirected>::new_undirected();
+    
+    let mut portals = grid.iter()
+        .flat_map(|(p, v)| {
+            if v.is_alphabetic() {
+                let dirs = [Dir::North, Dir::South, Dir::East, Dir::West];
+                for d in &dirs {
+                    let new_p = point_dir(p, d);
+                    if let Some('.') = grid.get(&new_p) {
+                        let opposite = p - (new_p - p);
+                        if let Some(n) = grid.get(&opposite) {
+                            if n.is_alphabetic() {
+                                let mut s = [(opposite, *n), (*p, *v)];
+                                s.sort_by(|a, b| {
+                                    let a = a.0;
+                                    let b = b.0;
+                                    match a.x.cmp(&b.x) {
+                                        std::cmp::Ordering::Equal => a.y.cmp(&b.y),
+                                        e => e
+                                    }
+                                });
+                                let s = s.iter().map(|(p, c)| c).collect::<String>();
+                                return Some((Tile::Portal(s), vec![new_p]));
+                            }
+                        }
+                    }
+                }
+            }
+            None
+        })
+        .collect::<Vec::<_>>();
+
+    //println!("Portals {:?}", portals);
+
+    println!("{} {}", max_x, max_y);
+    let outer = move |p :&Point2<_>| {
+        p.x < 4 || p.y < 4 || p.x > max_x - 4 || p.y > max_y - 4
+    };
+    
+    use itertools::Itertools;
+    portals.sort_by_key(|x| x.0.clone());
+    let portal_edges = portals.into_iter().coalesce(
+        |mut x, mut y| {
+            if x.0 == y.0 {
+                x.1.extend(y.1);
+                Ok(x)
+            } else {
+                Err((x,y))
+            }
+        })
+        .collect::<Vec<_>>();
+    use petgraph::graph::NodeIndex;
+    
+    fn recurse(outer : &dyn Fn(&Point2<i32>) -> bool, deps: &mut Graph<Tile,usize, petgraph::Undirected>, grid: &HashMap<Point2<i32>, char>,
+               portal_edges : &[(Tile, Vec<Point2<i32>>)],
+               all_nodes: &mut HashMap<(u32, Point2<i32>), NodeIndex<u32>>, depth: u32, max_depth: u32)
+    {
+        if depth > max_depth {
+            return;
+        }
+        
+        all_nodes.extend(
+            grid.iter().filter(|(k, v)| **v == '.')
+                .map(|(k, v)| ((depth, *k), (deps.add_node(Tile::Empty))))
+        );
+
+        
+        //println!("{:?}", all_nodes);
+        for (k, v) in grid.iter() {            
+            let east = point_dir(k, &Dir::East);
+            let north = point_dir(k, &Dir::North);
+
+            if let Some(n) = all_nodes.get(&(depth, *k)) {
+                for d in [east, north].iter() {
+                    if let Some(n2) = all_nodes.get(&(depth, *d)) {
+                        deps.add_edge(*n, *n2, 1);
+                    }
+                }
+            }
+        }
+        
+        let lower = depth + 1;
+        recurse(outer, deps, grid, portal_edges, all_nodes, lower, max_depth);
+        for (k,v) in portal_edges.iter() {                
+            if v.len() == 2 {
+                let (outer, inner) : (Vec<_>, Vec<_>) = v.iter().map(|x| *x).partition(outer);
+                match (all_nodes.get(&(lower, outer[0])),
+                       all_nodes.get(&(depth, inner[0]))) {
+                    (Some(lower), Some(this)) => {
+                        deps.add_edge(*lower, *this, 1);},
+                    _ => {},
+                }
+            }
+        }
+    }
+
+    let mut all_nodes = HashMap::new();
+    recurse(&outer, &mut deps, &grid, &portal_edges, &mut all_nodes, 0, 256);
+               
+    let aa = portal_edges.iter().find_map(
+        |(p, v)|
+        if *p == Tile::Portal("AA".to_string()) {
+            Some(*all_nodes.get(&(0, v[0])).unwrap())
+        } else {
+            None
+        }).unwrap();
+    
+    let zz = portal_edges.iter().find_map(
+        |(p, v)|
+        if *p == Tile::Portal("ZZ".to_string()) {
+            Some(*all_nodes.get(&(0, v[0])).unwrap())
+        } else {
+            None
+        }).unwrap();
+    
+    use petgraph::algo::astar;
+    if let Some((v, _)) =  astar(&deps, aa, |x| x == zz, |e| *e.weight(), |_| 0) {
+        println!("Part 2: {:?}", v);
+    } else {
+        println!("No Path");
+    }
+    Ok(())
 }
 
 #[cfg(test)]
