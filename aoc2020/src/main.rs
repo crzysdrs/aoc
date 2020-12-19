2847db8015eadf0e063ddfb5a6d60b36aa802f4a
Mitch Souders <mitch.souders@gmail.com>
2020-12-18 22:24:16 -0800

    Day 19

--- aoc2020/src/main.rs
+++ aoc2020/src/main.rs
@@ -59,6 +59,7 @@ mod day15;
 mod day16;
 mod day17;
 mod day18;
+mod day19;
 mod day2;
 mod day3;
 mod day4;
@@ -84,7 +85,7 @@ fn main() -> std::io::Result<()> {
 
     let sols = tests!(
         day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14,
-        day15, day16, day17, day18
+        day15, day16, day17, day18, day19
     );
 
     if let Some(sol) = sols.iter().find(|s| s.0 == opts.test) {
