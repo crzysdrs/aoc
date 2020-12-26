75c81d58ee1c13cde9025402055d5c53d4eddad9
Mitch Souders <mitch.souders@gmail.com>
2020-12-26 11:19:10 -0800

    Day 25

---
+++ aoc2020/src/day25.rs
@@ -0,0 +1,60 @@
+use crate::Day;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::Result as IoResult;
+
+
+fn transform(mut v: usize, sub_num: usize) -> usize {
+    v *= sub_num;
+    v.rem_euclid(20201227)
+}
+
+pub struct Solution {}
+impl Day for Solution {
+    const DAY: u32 = 25;
+    type Input = usize;
+    type Sol1 = usize;
+    type Sol2 = usize;
+
+    fn process_input<R>(r: R) -> IoResult<Vec<Self::Input>>
+    where
+        R: std::io::BufRead,
+    {
+        Ok(r.lines().flatten().map(|l| l.parse().unwrap()).collect())
+    }
+    fn p1(v: &[Self::Input]) -> Self::Sol1 {
+        let mut loops = v.iter().map(|sub| {
+            (0..).scan(1, |state, _| {
+                *state = transform(*state, 7);
+                Some(*state)
+            }).position(|v| v== *sub).unwrap() + 1
+        }).collect::<Vec<_>>();
+
+        loops.reverse();
+        let keys = v.iter().zip(loops.iter()).map(|(sub, loop_num)| {
+            let mut v = 1;
+            for _ in 0..*loop_num {
+                v = transform(v, *sub)
+            }
+            v
+        }).collect::<Vec<_>>();
+
+        assert_eq!(keys[0], keys[1]);
+        keys[0]
+    }
+    fn p2(_v: &[Self::Input]) -> Self::Sol2 {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test() {
+        let s = "5764801
+17807724";
+        let v = Solution::process_input(std::io::BufReader::new(s.as_bytes())).unwrap();
+        assert_eq!(Solution::p1(&v), 14897079);
+    }
+}
--- aoc2020/src/main.rs
+++ aoc2020/src/main.rs
@@ -66,6 +66,7 @@ mod day21;
 mod day22;
 mod day23;
 mod day24;
+mod day25;
 mod day3;
 mod day4;
 mod day5;
@@ -90,7 +91,7 @@ fn main() -> std::io::Result<()> {
 
     let sols = tests!(
         day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14,
-        day15, day16, day17, day18, day19, day20, day21, day22, day23, day24
+        day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
     );
 
     if let Some(sol) = sols.iter().find(|s| s.0 == opts.test) {
