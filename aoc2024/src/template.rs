e931b36f18142f885f7fbdd4f97a7f4497e07f38
Mitch Souders <mitch.souders@gmail.com>
2024-11-30 14:05:58 -0800

    Add 2024

---
+++ aoc2024/src/template.rs
@@ -0,0 +1,28 @@
+use crate::Day;
+#[allow(unused_imports)]
+use std::collections::*;
+
+pub struct Solution {}
+impl Day for Solution {
+    const DAY: u32 = 9999;
+    type Input1 = ();
+    type Input2 = ();
+    type Sol1 = usize;
+    type Sol2 = usize;
+
+    fn process_input1(_s: &str) -> Self::Input1 {
+        unimplemented!()
+    }
+    fn process_input2(_s: &str) -> Self::Input2 {
+        unimplemented!()
+    }
+    fn p1(_v: &Self::Input1) -> Self::Sol1 {
+        unimplemented!()
+    }
+    fn p2(_v: &Self::Input2) -> Self::Sol2 {
+        unimplemented!()
+    }
+}
+
+//crate::default_tests!((), ());
+//crate::string_tests!([(foo_sol1, "hi1", 0)], [(foo_sol2, "hi2", 1)]);
