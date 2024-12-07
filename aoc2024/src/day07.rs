95574571407520702ed9194c18982f7c286dfbae
Mitch Souders <mitch.souders@gmail.com>
2024-12-06 21:16:58 -0800

    Day 7 Part 2

--- aoc2024/src/day07.rs
+++ aoc2024/src/day07.rs
@@ -10,7 +10,7 @@ pub struct Solution {}
 impl Day for Solution {
     const DAY: u32 = 7;
     type Input1 = Vec<Test>;
-    type Input2 = ();
+    type Input2 = Vec<Test>;
     type Sol1 = usize;
     type Sol2 = usize;
 
@@ -28,8 +28,8 @@ impl Day for Solution {
             })
             .collect()
     }
-    fn process_input2(_s: &str) -> Self::Input2 {
-        unimplemented!()
+    fn process_input2(s: &str) -> Self::Input2 {
+        Self::process_input1(s)
     }
     fn p1(v: &Self::Input1) -> Self::Sol1 {
         v.iter()
@@ -55,8 +55,31 @@ impl Day for Solution {
             .map(|v| v.test)
             .sum()
     }
-    fn p2(_v: &Self::Input2) -> Self::Sol2 {
-        unimplemented!()
+    fn p2(v: &Self::Input2) -> Self::Sol2 {
+        v.iter()
+            .filter(|v| {
+                for x in 0..3usize.pow((v.values.len() - 1) as u32) {
+                    let result = v.values.iter().skip(1).enumerate().fold(
+                        v.values[0],
+                        |mut state, (idx, v)| {
+                            let op = (x / 3usize.pow(idx as u32)) % 3;
+                            match op {
+                                0 => state *= v,
+                                1 => state += v,
+                                2 => state = format!("{}{}", state, v).parse().unwrap(),
+                                _ => panic!(),
+                            }
+                            state
+                        },
+                    );
+                    if result == v.test {
+                        return true;
+                    }
+                }
+                false
+            })
+            .map(|v| v.test)
+            .sum()
     }
 }
 
