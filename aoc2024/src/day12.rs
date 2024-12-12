c3a9959f2a2f2f4b962ccf66af9152efe75d007a
Mitch Souders <mitch.souders@gmail.com>
2024-12-11 21:29:20 -0800

    Day 12 part 1

---
+++ aoc2024/src/day12.rs
@@ -0,0 +1,133 @@
+use crate::Day;
+use cgmath::{Point2, Vector2};
+#[allow(unused_imports)]
+use std::collections::*;
+
+pub const LEFT: Vector2<i32> = Vector2::new(-1, 0);
+pub const RIGHT: Vector2<i32> = Vector2::new(1, 0);
+pub const UP: Vector2<i32> = Vector2::new(0, 1);
+pub const DOWN: Vector2<i32> = Vector2::new(0, -1);
+
+pub struct Solution {}
+impl Day for Solution {
+    const DAY: u32 = 12;
+    type Input1 = HashMap<Point2<i32>, char>;
+    type Input2 = ();
+    type Sol1 = usize;
+    type Sol2 = usize;
+
+    fn process_input1(s: &str) -> Self::Input1 {
+        s.lines()
+            .enumerate()
+            .flat_map(|(y, s)| {
+                s.chars()
+                    .enumerate()
+                    .filter(|(_x, c)| *c != '.')
+                    .map(move |(x, c)| (Point2::new(x as i32, y as i32), c))
+            })
+            .collect()
+    }
+    fn process_input2(_s: &str) -> Self::Input2 {
+        unimplemented!()
+    }
+    fn p1(v: &Self::Input1) -> Self::Sol1 {
+        let mut seen = HashSet::new();
+        let mut region: HashMap<Point2<i32>, Region> = HashMap::new();
+
+        #[derive(Debug)]
+        struct Region {
+            plant: char,
+            start: Point2<i32>,
+            points: Vec<Point2<i32>>,
+        }
+
+        impl Region {
+            fn area(&self) -> usize {
+                self.points.len()
+            }
+            fn perimeter(&self, pts: &HashMap<Point2<i32>, char>) -> usize {
+                self.points
+                    .iter()
+                    .map(|p| {
+                        let dirs = [UP, DOWN, LEFT, RIGHT];
+                        let mut edge = 0;
+                        for d in dirs {
+                            match pts.get(&(p + d)) {
+                                Some(c) if *c != self.plant => {
+                                    edge += 1;
+                                }
+                                Some(_) => {}
+                                None => {
+                                    edge += 1;
+                                }
+                            }
+                        }
+                        edge
+                    })
+                    .sum()
+            }
+            fn flood_fill(
+                &mut self,
+                pts: &HashMap<Point2<i32>, char>,
+                seen: &mut HashSet<Point2<i32>>,
+            ) {
+                let dirs = [UP, DOWN, LEFT, RIGHT];
+                let mut worklist = VecDeque::new();
+                worklist.push_front(self.start);
+                self.points.push(self.start);
+                seen.insert(self.start);
+                while let Some(p) = worklist.pop_front() {
+                    for d in dirs {
+                        let new_p = p + d;
+                        if pts.get(&new_p) == Some(&self.plant) && seen.get(&new_p).is_none() {
+                            seen.insert(new_p);
+                            worklist.push_back(new_p);
+                            self.points.push(new_p);
+                        }
+                    }
+                }
+            }
+        }
+
+        let mut regions = vec![];
+        for (p, c) in v {
+            if seen.get(p).is_none() {
+                let mut r = Region {
+                    plant: *c,
+                    start: *p,
+                    points: vec![],
+                };
+
+                r.flood_fill(v, &mut seen);
+                regions.push(r);
+            }
+        }
+        //not 245473
+        println!(
+            "{:?}",
+            regions
+                .iter()
+                .map(|r| (r.plant, r.perimeter(&v), r.area()))
+                .collect::<Vec<_>>()
+        );
+        regions.iter().map(|r| r.perimeter(&v) * r.area()).sum()
+    }
+    fn p2(_v: &Self::Input2) -> Self::Sol2 {
+        unimplemented!()
+    }
+}
+
+//crate::default_tests!((), ());
+crate::string_tests!(
+    [(
+        foo_sol1,
+        "AAAA
+BBCD
+BBCC
+EEEC",
+        140
+    )],
+    [
+        //(foo_sol2, "hi2", 1)
+    ]
+);
